8cced299260fdfbaa4ede1f66be9cebe63c77d7c
Sergio Alberto Marquez Moreno <serdevic1926@gmail.com>
2020-03-26 00:09:29 -0600

    remove files

--- core/configGeneral.php
+++
@@ -1,5 +0,0 @@
-<?php
-    const SERVER = "http://192.168.100.79/plataformaifpp/";
-    const TITLEBAR = "Instituto de Formación Profesional de la Procuraduría General de Justicia del Estado de Hidalgo";
-    const COMPANY = "Instituto de Formación Profesional";
-?>
